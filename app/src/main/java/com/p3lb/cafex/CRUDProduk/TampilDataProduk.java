84ba9de3b6bac8f9343518afe295e2fdd311d913
niel_putra <niel.zebua8@gmail.com>
2021-03-05 10:09:31 +0700

    float button sudah ditambah

--- app/src/main/java/com/p3lb/cafex/CRUDProduk/TampilDataProduk.java
+++ app/src/main/java/com/p3lb/cafex/CRUDProduk/TampilDataProduk.java
@@ -1,13 +1,18 @@
 package com.p3lb.cafex.CRUDProduk;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.p3lb.cafex.InvoiceActivity;
 import com.p3lb.cafex.R;
+import com.p3lb.cafex.TambahProdukActivity;
 import com.p3lb.cafex.adapter.ProdukAdapter;
 import com.p3lb.cafex.model.ProdukDAO;
 import com.p3lb.cafex.network.ApiHelper;
@@ -25,16 +30,33 @@ public class TampilDataProduk extends AppCompatActivity {
     private RecyclerView recyclerView;
     ProdukAdapter produkAdapter;
     List<ProdukDAO> ProdukDAO;
-
+    public FloatingActionButton fltTambahProduk, fltKeranjangBelanja;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_dataproduk);
+        fltTambahProduk = findViewById(R.id.btnTambahProduk);
+        fltKeranjangBelanja = findViewById(R.id.btnKeranjangBelanja);
         ProdukDAO = new ArrayList<>();
         recyclerView = findViewById(R.id.recycleView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         getData();
+        fltTambahProduk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TampilDataProduk.this, TambahProdukActivity.class);
+                startActivity(intent);
+            }
+        });
+        fltKeranjangBelanja.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TampilDataProduk.this, InvoiceActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
